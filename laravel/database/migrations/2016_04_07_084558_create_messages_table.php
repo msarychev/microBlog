40007e30247c9dc317acfa0dd88e927cfb31d01a
quantros <root@redlinedevelop.ru1>
2016-04-07 06:51:54 +0600

    init models

---
+++ laravel/database/migrations/2016_04_07_084558_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('messages', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('user');
+			$table->text('text');
+			$table->integer('likes');
+			$table->timestamp('created_at')->nullable();
+			$table->timestamp('updated_at')->nullable();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('messages');
+	}
+
+}
---
+++ laravel/database/migrations/2016_04_07_094146_create_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachmentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('attachments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('message_id');
+			$table->enum('Type', array('picture', 'link', 'video'));
+			$table->timestamp('created_at')->nullable();
+			$table->timestamp('updated_at')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('attachments');
+	}
+
+}
